ec1295427a0cb779eebec08f66e8433e2acc8de2
FarmaczevT <dienis.ghorn@mail.ru>
2024-10-20 19:11:55 +0500

    убрал перезапись рефреш токена

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -43,6 +43,12 @@
                     // Сохраняем обновленный массив токенов в localStorage
                     localStorage.setItem('auth_tokens', JSON.stringify(tokens));
                 }
+                
+                // Проверяем, существует ли refresh_token перед его сохранением
+                const existingRefreshToken = localStorage.getItem('refresh_token');
+                    if (!existingRefreshToken) {
+                        localStorage.setItem('refresh_token', data.refresh_token);
+                    }
 
                 alert('Успешная авторизация!');
             } else {
